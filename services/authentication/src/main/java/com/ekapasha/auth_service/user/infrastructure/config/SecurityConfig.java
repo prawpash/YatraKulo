c7688ade338b6efb854db5be896d26576f88136e
pasha <robbyprawira3@gmail.com>
2026-02-17 20:00:11 +0700

    feat(security): add RSA jwk support and inject RSA properties
    Introduce RSA key handling and JWK infrastructure to support JWT
    operations and future authorization server setup.

--- services/authentication/src/main/java/com/ekapasha/auth_service/user/infrastructure/config/SecurityConfig.java
+++ services/authentication/src/main/java/com/ekapasha/auth_service/user/infrastructure/config/SecurityConfig.java
@@ -1,50 +1,35 @@
 package com.ekapasha.auth_service.user.infrastructure.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        .sessionManagement(
-            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(
-            auth ->
-                auth
-                    // Public endpoints
-                    .requestMatchers("/actuator/health")
-                    .permitAll()
-                    .requestMatchers("/v3/api-docs/**", "/scalar/**")
-                    .permitAll()
+  private final RsaProperties rsaProperties;
 
-                    // Protected endpoints
-                    .anyRequest()
-                    .authenticated())
-        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
-        .exceptionHandling(
-            ex -> ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
-
-    return http.build();
-  }
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
@@ -57,5 +42,65 @@ public class SecurityConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
+  }
+
+  @Bean
+  public JWKSource<SecurityContext> jwkSource() {
+    RSAPublicKey publicKey = this.parseRSAPublicKey(this.rsaProperties.getPublicKey());
+    RSAPrivateKey privateKey = this.parseRSAPrivateKey(this.rsaProperties.getPrivateKey());
+
+    RSAKey rsaKey =
+        new RSAKey.Builder(publicKey)
+            .privateKey(privateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build();
+    JWKSet jwkSet = new JWKSet(rsaKey);
+    return new ImmutableJWKSet<>(jwkSet);
+  }
+
+  /**
+   * Parses the public key from the PEM format.
+   *
+   * @param publicKey the public key in PEM format
+   * @return the public key
+   */
+  private RSAPublicKey parseRSAPublicKey(String publicKey) {
+    try {
+      String publicKeyContent =
+          publicKey
+              .replace("-----BEGIN PUBLIC KEY-----", "")
+              .replace("-----END PUBLIC KEY-----", "")
+              .replaceAll("\\s", "");
+
+      byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyContent);
+      X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+    } catch (Exception e) {
+      throw new RuntimeException("Failded to parse public key", e);
+    }
+  }
+
+  /**
+   * Parses the private key from the PEM format.
+   *
+   * @param privateKey the private key in PEM format
+   * @return the private key
+   */
+  private RSAPrivateKey parseRSAPrivateKey(String privateKey) {
+    try {
+      String privateKeyContent =
+          privateKey
+              .replace("-----BEGIN PRIVATE KEY-----", "")
+              .replace("-----END PRIVATE KEY-----", "")
+              .replaceAll("\\s", "");
+
+      byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyContent);
+      PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+      KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+      return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
+    } catch (Exception e) {
+      throw new RuntimeException("Failded to parse private key", e);
+    }
   }
 }
