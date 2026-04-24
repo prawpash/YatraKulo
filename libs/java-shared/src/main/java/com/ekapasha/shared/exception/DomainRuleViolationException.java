1ed98a6ff340d7099a30b1384e8727a0afa62e0d
pasha <robbyprawira3@gmail.com>
2026-04-24 23:41:19 +0700

    refactor: reorganize exception packages
    Move exception classes from auth_service-specific package to
    shared exception package.

--- libs/java-shared/src/main/java/com/ekapasha/shared/exception/DomainRuleViolationException.java
+++ libs/java-shared/src/main/java/com/ekapasha/shared/exception/DomainRuleViolationException.java
@@ -1,4 +1,4 @@
-package com.ekapasha.auth_service.shared.domain.exception;
+package com.ekapasha.shared.exception;
 
 public class DomainRuleViolationException extends RuntimeException {
   public DomainRuleViolationException(String message) {
