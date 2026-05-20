a871c6d2697a21eee96c6abe2a9b6103e2002444
prawpash <robbyprawira3@gmail.com>
2026-05-20 22:32:11 +0700

    feat: add jacoco to handle test coverage

--- services/authentication/build.gradle.kts
+++ services/authentication/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
+    jacoco
 }
 
 group = "com.ekapasha"
@@ -63,4 +64,12 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
