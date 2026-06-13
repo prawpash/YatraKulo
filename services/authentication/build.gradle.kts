b8dec5bd5a6432fb4a4c8bfd03f561f69186aac7
pasha <robbyprawira3@gmail.com>
2026-06-13 23:17:24 +0700

    feat(opentelemetry): add OTLP tracing and opentelemetry config
    Enable Telemetry tracing and add related dependency to exporting
    application traces to an OTLP collector.

--- services/authentication/build.gradle.kts
+++ services/authentication/build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
     // Source: https://mvnrepository.com/artifact/org.springframework/spring-jcl
     implementation("org.springframework:spring-jcl:6.2.15")
 
+    // Source: https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-opentelemetry
+    implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
+
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
